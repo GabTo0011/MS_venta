719627189c48724d6f5b8b7961bc9ebc3c0891d0
camilo_workspace_model/controller/service <camilovsprogram355@gmail.com>
2025-06-11 23:03:53 -0400

    Cambia el VentaDTO (no guardo antes)

--- gestion-venta/src/main/java/com/perfulandia/venta/dto/VentaDTO.java
+++ gestion-venta/src/main/java/com/perfulandia/venta/dto/VentaDTO.java
@@ -2,14 +2,16 @@ package com.perfulandia.venta.dto;
 
 import lombok.*;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class VentaDTO {
-    private Integer idVenta;
-    private Integer idCliente;
-    private Integer idVendedor;
-    private Date fechaVenta;
+    private Integer id;
+    private LocalDateTime fechaVenta;
+    private String nombreCliente;
+    private Double total;
+    private List<DetalleVentaDTO> detalles;
 }
