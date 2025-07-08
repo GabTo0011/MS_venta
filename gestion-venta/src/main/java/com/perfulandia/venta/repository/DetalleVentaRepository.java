d05eda6215556b0bef38ebc2da7b93f5d733b2c3
Gabriel T <gab.toledo@duocuc.cl>
2025-07-08 17:29:15 -0400

    refactor(venta): Eliminar importación no usada en VentaService
    Este commit comenta una sentencia de importación de repositorio no utilizada en VentaService.
    No se hicieron cambios funcionales; es una limpieza de código.

--- gestion-venta/src/main/java/com/perfulandia/venta/repository/DetalleVentaRepository.java
+++ gestion-venta/src/main/java/com/perfulandia/venta/repository/DetalleVentaRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 
 @Repository
 public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Integer> {
+    
     List<DetalleVenta> findByVenta_IdVenta(Integer idVenta);
 }
