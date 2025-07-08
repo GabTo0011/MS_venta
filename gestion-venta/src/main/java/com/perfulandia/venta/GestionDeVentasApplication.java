e3a04241ca6f65f3f0c2568fe83c8645073f72b9
Gabriel T <gab.toledo@duocuc.cl>
2025-07-08 17:46:01 -0400

    feat(config): Configurar escaneo de paquetes base y actualizar nombre de BD
    Este commit actualiza GestionDeVentasApplication para incluir `scanBasePackages = {com.perfulandia}` para un escaneo de componentes adecuado.
    Adicionalmente, se cambió el nombre de la base de datos en `application.properties` para reflejar el mapeo correcto del proyecto.

--- gestion-venta/src/main/java/com/perfulandia/venta/GestionDeVentasApplication.java
+++ gestion-venta/src/main/java/com/perfulandia/venta/GestionDeVentasApplication.java
@@ -3,7 +3,7 @@ package com.perfulandia.venta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.perfulandia"})
 public class GestionDeVentasApplication {
 
 	public static void main(String[] args) {
